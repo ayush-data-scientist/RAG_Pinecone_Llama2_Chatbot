b3a085d07512a93d86b7c3ca04449beaa88e0605
ayush-data-scientist <ayushsaxena241195.official@gmail.com>
2025-06-27 14:22:43 +0530

    setup file added to find local package with construcuctor __init__ func

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="rag_pinecone_llama2_chatbot",
+    version='0.0.1',
+    author="Ayush Saxena",
+    author_email="ayushsaxena241195.official@gmil.com",
+    description="A chatbot application using RAG with Pinecone and Llama2",
+    packages=find_packages(),
+    install_requires=[]
+)
